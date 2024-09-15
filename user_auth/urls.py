949923e6a53516a0535fac917c0d066be73240c6
Hadise.M <Hadisemogahdam8@gamil.com>
2024-09-15 17:47:44 +0330

    fix : define a URL pattern for the root URL

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,7 +1,9 @@
 # to add the URLs for the login, register, and test views 
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),  # add this line
     path('tokens/', include('tokens.urls')),
     path('authenticate/', include('authenticate.urls')),
     path('test/', include('tokens.urls')),  # for testing with access token
